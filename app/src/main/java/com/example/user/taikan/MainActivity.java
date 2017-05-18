41e2f82f1db8d9fa4019a1c18be5c5fd239fa3c3
USER <omegala8@gmail.com>
2017-05-18 19:48:19 +0900

    ref #17 カレンダービュー作成

---
+++ app/src/main/java/com/example/user/taikan/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.taikan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView)findViewById(R.id.calendarView);
+    }
+}
